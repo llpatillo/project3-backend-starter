00194f83d8de237b1ab8203bfd265f5d7361c7ac
Kyshia Patillo <llpatillo@yahoo.com>
2019-11-26 12:37:47 -0500

    replace candidate image with candidate campaign video and change comment to string

--- seeders/20191121171313-candidate.js
+++ seeders/20191121171313-candidate.js
@@ -5,7 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert('Candidates', [
       {
        name:"Joe Biden",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/17a0c0f/2147483647/strip/true/crop/1024x671+0+0/resize/1024x671!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2Fe0%2Ff5%2F837073dc436cb03abc7306fff07c%2Fcandidates-gallery-biden-getty-042519.jpg",
+       candidate_img_url:"https://www.youtube.com/watch?v=VbOU2fTg6cI",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"None",
@@ -19,7 +19,7 @@ module.exports = {
      },
      {
        name:"Cory Booker",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/2fbb835/2147483647/strip/true/crop/1024x683+0+0/resize/1024x683!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F82%2Fd6%2F469a74db48ca92c01f75f102fd1b%2F2020-candidate-gallery-booker.jpg",
+       candidate_img_url:"https://www.youtube.com/watch?v=mx5m6DDFupg",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"U.S. Senator",
@@ -33,7 +33,7 @@ module.exports = {
      },
      {
        name:"Pete Buttigieg",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/4f975ff/2147483647/strip/true/crop/1024x683+0+0/resize/1024x683!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F08%2F44%2F6bc858cd45fea2598f12ce208317%2Fcandidates-gallery-buttegeig-pete-042519.jpg",
+       candidate_img_url:"https://www.youtube.com/watch?v=3uOly3jGCBo",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"Mayor, South Bend, IN",
@@ -47,7 +47,7 @@ module.exports = {
      },
      {
        name:"Rocky De La Fuente",
-       candidate_img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLaQsQ0GgttvjMzN8n_E47Ydsu3CaIf1VUB3WJn24ucQj9Z_ir&s",
+       candidate_img_url:"https://www.youtube.com/watch?v=LW5WJJXEzHA",
        political_party:"Republican",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Republican_Disc.svg/300px-Republican_Disc.svg.png",
        current_office:"None",
@@ -61,7 +61,7 @@ module.exports = {
      },
      {
        name:"Tulsi Gabbard",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/6b8c682/2147483647/strip/true/crop/3046x2031+0+0/resize/2160x1440!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2Fc7%2F34%2Fd39a1cef4318a8b5511e1eb2d978%2F2020-candidate-gallery-tulsi-gabbard.jpg",
+       candidate_img_url:"https://youtu.be/c0pCTjRIIhk",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"U.S. State Representative, Hawaii",
@@ -75,7 +75,7 @@ module.exports = {
      },
      {
        name:"Kamala Harris",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/78ba77d/2147483647/strip/true/crop/3200x2133+0+0/resize/2160x1440!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2Fb2%2Ff6%2F8deabf8c493ba132ef1decc91704%2F2020-candidate-gallery-kamala-harris.jpg",
+       candidate_img_url:"https://www.youtube.com/watch?v=Ls7OSwHMoBc",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"U.S. Senator, California",
@@ -89,7 +89,7 @@ module.exports = {
      },
      {
        name:"Amy Klobuchar",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/3a67c37/2147483647/strip/true/crop/1024x683+0+0/resize/1024x683!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F63%2Fbd%2Fb3b2cbd84082990457f470bf1411%2Fgettyimages-1042679684.jpg",
+       candidate_img_url:"https://www.youtube.com/watch?v=vghXHCAGcIw",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"U.S. Senator, Minnesota",
@@ -103,7 +103,7 @@ module.exports = {
      },
      {
        name:"Bernie Sanders",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/787a70d/2147483647/strip/true/crop/1024x683+0+0/resize/1024x683!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F8b%2F7d%2Ffdc95e38417fbb62aefeaa62d56a%2Fgettyimages-1085911782.jpg",
+       candidate_img_url:"https://www.youtube.com/watch?v=s7DRwz0cAt0",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"U.S. Sentaor (2006 - present)",
@@ -117,7 +117,7 @@ module.exports = {
      },
      {
        name:"Tom Steyer",
-       candidate_img_url:"https://static01.nyt.com/images/2019/09/08/us/politics/08debate/merlin_160363629_bc047c74-14b5-4491-88cb-673c519409b1-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+       candidate_img_url:"https://youtu.be/Q0pFvLtryd0",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"Governor, South Carolina",
@@ -131,7 +131,7 @@ module.exports = {
      },
      {
        name:"Donald Trump",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/5432cc5/2147483647/strip/true/crop/1024x683+0+0/resize/1024x683!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F4a%2F8c%2F64d8026d438ca99dfa37c6d0dc36%2F2020-candidate-gallery-donald-trump.jpg",
+       candidate_img_url:"https://youtu.be/aIrtAASuMbQ",
        political_party:"Republican",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Republican_Disc.svg/300px-Republican_Disc.svg.png",
        current_office:"U.S. President",
@@ -145,7 +145,7 @@ module.exports = {
      },
      {
        name:"Elizabeth Warren",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/d04ea69/2147483647/strip/true/crop/1024x683+0+0/resize/1024x683!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2Fa0%2F1b%2F6b90dd994684ab9e5af9d841be8b%2F2020-candidate-gallery-warren-021119.jpg",
+       candidate_img_url:"https://youtu.be/uF3IjYG8eeY",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"U.S. Senator, Massachusetts",
@@ -159,7 +159,7 @@ module.exports = {
      },
      {
       name:"Bill Weld",
-      candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/b412fd8/2147483647/strip/true/crop/3288x4753+0+0/resize/996x1440!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F86%2F2e%2F7fc0bc6d41478284c5f7016d713b%2F2020-candidates-gallery-bill-weld.jpg",
+      candidate_img_url:"https://youtu.be/wbQzEXzsygA",
       political_party:"Republican",
       political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Republican_Disc.svg/300px-Republican_Disc.svg.png",
       current_office:"None",
@@ -173,7 +173,7 @@ module.exports = {
     },
     {
        name:"Andrew Yang",
-       candidate_img_url:"https://ewscripps.brightspotcdn.com/dims4/default/f0298e0/2147483647/strip/true/crop/1044x667+0+0/resize/1044x667!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0e%2F92%2F47fec20945eea31d2dc094fb84a8%2F2020-candidate-gallery-andrew-yang.png",
+       candidate_img_url:"https://youtu.be/EgQb2NNQ43w",
        political_party:"Democrat",
        political_party_img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/300px-US_Democratic_Party_Logo.svg.png",
        current_office:"None",
